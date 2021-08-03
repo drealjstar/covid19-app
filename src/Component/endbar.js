15968d202cc505044158cc9b0ff64271d2ed8385
jstar <drealjstar@yahoo.com>
2021-08-03 05:57:06 -1200

    arrangement of code

---
+++ src/Component/endbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./endbar.css";
+
+
+class Endbar extends Component {
+    render() {
+        return (
+            <div className="end_bar">
+                <h3>CONTACT</h3>
+                <h3>POLICIES</h3>
+                <h3>ABOUT</h3>
+                <h3>SOCIAL MEDIA</h3>
+                <h3>DIRECTORIES</h3>
+            </div>
+        );
+    }
+}
+
+
+export default Endbar;

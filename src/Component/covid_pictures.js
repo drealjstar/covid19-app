a1224b9ef48c74c981f577bc8daea39e24ea25b9
jstar <drealjstar@yahoo.com>
2021-08-02 22:29:15 -1200

    additional changes

--- src/Component/covid_pictures.js
+++ src/Component/covid_pictures.js
@@ -1,11 +1,11 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./covid_pictures.css";
 
 
 
-class Album extends Component{
-    render(){
-        return(
+class Album extends Component {
+    render() {
+        return (
             <div className="covid_pictures">
 
 
--- src/Component/stat.js
+++ src/Component/stat.js
@@ -1,63 +1,62 @@
-import React, {Component} from  "react";
+import React, { Component } from "react";
 import Axios from "axios";
 import "./stat.css";
 
 class Stat extends Component {
-    state = {
-       
-     stat : []
- 
-    }
- 
-    componentDidMount() {
-     Axios.get("https://corona.lmao.ninja/v2/countries?sort=country").then((result) => {
-    console.log(result, "iiiiuuu");
- 
-       this.setState({ stat: result.data[73]});
-     });
-   }
- 
- 
-  
-render (){
- 
-    const {stat} = this.state;
+  state = {
 
-     return(
-     <div className="home_page">
-     <div className="image"><h1 className="title">NIGERIA CORONAVIRUS UPDATE</h1></div>
-       
-      <div className="statistics" >
+    stat: []
 
-      <div className="total_population">
-        <h2>POPULATION</h2>
-      <div className="nigeria"><h1>{stat.population}</h1></div>
-      </div>
-      
-      <div className="total_infected">
-      <h2>CASES</h2>
-      <div className="infected"><h1>{stat.cases}</h1></div>
-       </div>
+  }
 
-      <div className="total_recovering">
-      <h2>RECOVERED</h2>
-      <div className="recovered"><h1>{stat.recovered}</h1></div>
-      </div>
+  componentDidMount() {
+    Axios.get("https://corona.lmao.ninja/v2/countries?sort=country").then((result) => {
+      console.log(result, "iiiiuuu");
 
-      <div className="total_death">
-      <h2>DEATH</h2>
-      <div className="death"><h1>{stat.deaths}</h1></div>
-      </div>
+      this.setState({ stat: result.data[73] });
+    });
+  }
 
-      <div className="active_cases">
-      <h2>ACTIVE CASES</h2>
-      <div className="active"><h1>{this.state.stat.active}</h1></div>
-      </div>
 
+  render() {
+
+    const { stat } = this.state;
+
+    return (
+      <div className="home_page">
+        <div className="image"><h1 className="title">NIGERIA CORONAVIRUS UPDATE</h1></div>
+
+        <div className="statistics" >
+
+          <div className="total_population">
+            <h2>POPULATION</h2>
+            <div className="nigeria"><h1>{stat.population}</h1></div>
+          </div>
+
+          <div className="total_infected">
+            <h2>CASES</h2>
+            <div className="infected"><h1>{stat.cases}</h1></div>
+          </div>
+
+          <div className="total_recovering">
+            <h2>RECOVERED</h2>
+            <div className="recovered"><h1>{stat.recovered}</h1></div>
+          </div>
+
+          <div className="total_death">
+            <h2>DEATH</h2>
+            <div className="death"><h1>{stat.deaths}</h1></div>
+          </div>
+
+          <div className="active_cases">
+            <h2>ACTIVE CASES</h2>
+            <div className="active"><h1>{this.state.stat.active}</h1></div>
+          </div>
+
+        </div>
       </div>
-      </div>
-      );
-   }
- }
- 
- export default Stat;
+    );
+  }
+}
+
+export default Stat;

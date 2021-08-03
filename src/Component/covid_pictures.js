e2c1f16e104ef058b0347ab8c478b18de1968131
jstar <drealjstar@yahoo.com>
2021-08-02 22:15:18 -1200

    add all changes

---
+++ src/Component/covid_pictures.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import "./covid_pictures.css";
+
+
+
+class Album extends Component{
+    render(){
+        return(
+            <div className="covid_pictures">
+
+
+
+            </div>
+        )
+    }
+}
+
+export default Album;

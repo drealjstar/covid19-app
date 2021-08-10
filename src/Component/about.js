f3b02cd6a3b9d68ba512aabaf1e14d52a8cc0baa
jstar <drealjstar@yahoo.com>
2021-08-10 03:46:26 -1200

    adding services

--- src/Component/about.js
+++ src/Component/about.js
@@ -6,7 +6,7 @@ class About extends Component {
     render() {
         return (
             <div className="about">
-
+                <div className="about_link">
                 <Link to="/" className="stat3">
                     <p className="stat">HOME</p>
                 </Link>
@@ -19,9 +19,37 @@ class About extends Component {
                     <p className="pol">POLICY</p>
                 </Link>
 
-                <Link to="/directory" className="directory4">
-                    <p className="dir">DIRECTORY</p>
+                <Link to="/services" className="services4">
+                    <p className="ser">SERVICES</p>
                 </Link>
+             </div>
+
+            <div className="intro">
+            
+            <p>Our Organization Dasent Health Centre the organizer of Dasent Covid-19 Centre </p>
+            <p>specialises in health care provision. providing adequate support to the people </p>
+            <p>with health challenges.</p>
+            </div>
+
+            <div className="vision">
+            <h2>Vision</h2>
+            <p>To be the greatest private health care organization with the highest Medical Facilities,</p>
+            <p>To create and improve medical environment to the safety of human. And get to the level of</p>
+            <p>government own hospitals.</p>
+            </div>
+
+            <div className="mission">
+            <h2>Mission</h2>
+            <p>We have provide the necessary and adquate equipments, highly qualify staffs for cuurent </p>
+            <p>success, which as well guarantee more success years to come. The organization made it</p>
+            <p>relevant aim and objective, regular assessesment to measure the rate of growth over time. </p>
+            </div>
+
+
+
+
+
+
 
             </div>
         );
--- src/Component/contact.js
+++ src/Component/contact.js
@@ -2,28 +2,42 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./contact.css";
 
+
 class Contact extends Component {
-    render() {
+
+render() {
         return (
             <div className="contact">
+            <div className="contact_info_list">
                 <Link to="/" className="stat2">
                     <p className="stat">HOME</p>
                 </Link>
-
+                
                 <Link to="/about" className="about3">
                     <p className="abo">ABOUT</p>
                 </Link>
-
+                
                 <Link to="/policy" className="policy3">
                     <p className="pol">POLICY</p>
                 </Link>
 
-                <Link to="/directory" className="directory3">
-                    <p className="dir">DIRECTORY</p>
+                <Link to="/services" className="services3">
+                    <p className="ser">SERVICES</p>
                 </Link>
-
-            </div>
-        );
+                 </div>
+                <div className="contact_info">
+                <div className="contact_details">
+                <h2>Dasent Covid-19 Centre</h2>
+                <p>234 Lekki Express Way, Opposite Manbas Complex </p>
+                <p>Lekki Ajah, Lagos.</p>
+                <p>website : www.dasenthealthcare.com</p>
+                <p>Email : dasentcentre@gmail.com</p>
+                <p>Phone : +2348012345678</p>
+                 </div>
+                <div className="contact_image"></div>
+        </div>
+        </div>
+     );
     }
 }
 
